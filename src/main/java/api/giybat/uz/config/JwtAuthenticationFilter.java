2e9331e78b4d4705713a42aa1c6a02380533b210
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2025-02-05 21:32:13 +0500

    auth finish

--- src/main/java/api/giybat/uz/config/JwtAuthenticationFilter.java
+++ src/main/java/api/giybat/uz/config/JwtAuthenticationFilter.java
@@ -56,6 +56,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
         }catch (JwtException | UsernameNotFoundException e){
             filterChain.doFilter(request, response);
             return;
