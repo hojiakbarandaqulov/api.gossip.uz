16246f92a279d66bb987842125794eef294387f7
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2025-04-23 20:01:52 +0500

    Profile Username code

--- src/main/java/api/giybat/uz/controller/ProfileController.java
+++ src/main/java/api/giybat/uz/controller/ProfileController.java
@@ -50,4 +50,6 @@ public class ProfileController {
         ApiResponse<String> apiResponse = profileService.updateUsernameConfirm(dto, language);
         return ResponseEntity.ok(apiResponse);
     }
+
+
 }
