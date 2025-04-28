e755d6d2bee67171282ff88501cc5a4ec6f494b6
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2025-04-28 21:04:23 +0500

    Profile updated

--- src/main/java/api/giybat/uz/Application.java
+++ src/main/java/api/giybat/uz/Application.java
@@ -10,5 +10,6 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 //		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 //		System.out.println(bCryptPasswordEncoder.encode("1234"));
+
 	}
 }
