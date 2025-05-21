983d144aecd79f28e68304d79adffbf4510da4e4
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2025-05-21 15:09:38 +0500

    profile update photo

--- src/main/java/api/giybat/uz/Application.java
+++ src/main/java/api/giybat/uz/Application.java
@@ -2,16 +2,9 @@ package api.giybat.uz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.UUID;
-
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		System.out.println(bCryptPasswordEncoder.encode("1234"));
-
 	}
 }
